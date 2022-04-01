482149507a44a1d7077a7aee1cd8adb2074c6c51
Markus <markus.specogna@gmail.com>
2022-04-01 14:23:16 -0400

    Modify dark/light mode
    Adjust dark/light mode with localStorage options

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,39 @@
 console.log('Hello, how\'s it going?')
 
-/*
-* World's most simple dark/light mode toggle
-*/
-const toggleButton = document.getElementById('toggle');
+// set up variables
+let darkMode = localStorage.getItem('darkMode')
+const modeToggle = document.getElementById('toggle')
 
-toggleButton.addEventListener('click', function () {
-    let body = document.getElementById('body')
-    body.classList.toggle('dark');
-    toggleButton.classList.toggle('dark');
+// enable dark mode
+// add/remove dark/light class from body
+// set localstorage darkMode === enabled
+const enableDarkMode = () => {
+    document.body.classList.add('dark')
+    document.body.classList.remove('light')
+    localStorage.setItem('darkMode', 'enabled')
+}
+
+// disable dark mode
+// add/remove light/dark class from body
+// set localstorage darkMode === null
+const disableDarkMode = () => {
+    document.body.classList.remove('dark')
+    document.body.classList.add('light')
+    localStorage.setItem('darkMode', null)
+}
+
+// Check if darkMode is equal to enabled, if so, do enableDarkMode()
+if (darkMode === 'enabled') {
+    enableDarkMode()
+}
+
+// When toggle is clicked, check/get localstorage darkMode, if it is not enabled, enable it, else, disable it
+modeToggle.addEventListener('click', () => {
+    darkMode = localStorage.getItem('darkMode')
+
+    if (darkMode !== 'enabled') {
+        enableDarkMode()
+    } else {
+        disableDarkMode()
+    }
 })
