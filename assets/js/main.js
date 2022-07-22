5d169e34e42a4f08df5666730371e74b1161ed6b
Markus <markus.specogna@gmail.com>
2022-07-22 09:16:06 -0400

    apply new transitions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,46 @@
-console.log('Hello, how\'s it going?')
+console.log("Hello, how's it going?");
 
 // set up variables
-let darkMode = localStorage.getItem('darkMode')
-const modeToggle = document.getElementById('toggle')
+let darkMode = localStorage.getItem('darkMode');
+const modeToggle = document.getElementById('toggle');
 
 // enable dark mode
 // add/remove dark/light class from body
 // set localstorage darkMode === enabled
 const enableDarkMode = () => {
-    document.body.classList.add('dark')
-    document.body.classList.remove('light')
-    localStorage.setItem('darkMode', 'enabled')
-}
+	document.body.classList.add('dark');
+	document.body.classList.remove('light');
+	localStorage.setItem('darkMode', 'enabled');
+};
 
 // disable dark mode
 // add/remove light/dark class from body
 // set localstorage darkMode === null
 const disableDarkMode = () => {
-    document.body.classList.remove('dark')
-    document.body.classList.add('light')
-    localStorage.setItem('darkMode', null)
-}
+	document.body.classList.remove('dark');
+	document.body.classList.add('light');
+	localStorage.setItem('darkMode', null);
+};
 
 // Check if darkMode is equal to enabled, if so, do enableDarkMode()
 if (darkMode === 'enabled') {
-    enableDarkMode()
+	enableDarkMode();
 }
 
 // When toggle is clicked, check/get localstorage darkMode, if it is not enabled, enable it, else, disable it
 modeToggle.addEventListener('click', () => {
-    darkMode = localStorage.getItem('darkMode')
+	darkMode = localStorage.getItem('darkMode');
 
-    if (darkMode !== 'enabled') {
-        enableDarkMode()
-    } else {
-        disableDarkMode()
-    }
-})
+	if (darkMode !== 'enabled') {
+		enableDarkMode();
+	} else {
+		disableDarkMode();
+	}
+});
+
+// Get current year for footer
+let year = document.getElementById('year');
+
+if (year) {
+	year.innerHTML = new Date().getFullYear() + '.';
+}
